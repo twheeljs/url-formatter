0213e207d1bc0288b4b6da87fd2c1dd885a9b367
yangjin <yangjin.im@bytedance.com>
2026-03-03 19:36:02 +0800

    feat: add web analytics

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,16 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+
 <template>
   <UApp>
     <div class="flex flex-col min-h-screen">
       <Navbar />
-      <div class="flex-1 page">
+      <div class="flex-1">
         <NuxtPage />
       </div>
       <Footer />
+      <Analytics />
     </div>
   </UApp>
 </template>
